c49478315163f9914a16e65b2670debb73ac2fe9
Daniel Romero <romerodaniel.k90@gmail.com>
2018-10-21 18:02:37 -0700

    boards data loads; needs: styling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <h1>the page</h1>
+<div class="main">
+  <Board />
+</div>
 </template>
 
 <script>
+
+import Board from './components/app/Board.vue'
+
 export default {
   name: "App",
   data: () => {
     return {
       mocks: [{ data: "data" }]
     };
+  },
+  components: {
+    Board
   }
 };
 </script>
---
+++ src/components/app/Board.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="boards">
+  <h1>Board</h1>
+  <pre>{{boards}}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Board",
+  data() {
+    return {
+      boards: []
+    };
+  },
+  mounted() {
+    this.getBoards();
+  },
+  methods: {
+    getBoards() {
+      return fetch("http://localhost:3001/boards")
+        .catch(e => console.error(e))
+        .then(data => data.json())
+        .then(boards => this.boards = boards);
+    }
+  }
+};
+</script>
+
